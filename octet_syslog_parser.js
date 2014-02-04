caf30f8428d009b2a5d78850aca54ac41cff24ba
Jonas Schneider <mail@jonasschneider.com>
2014-02-05 00:18:33 +0100

    init

---
+++ octet_syslog_parser.js
@@ -0,0 +1,51 @@
+var glossy = require('glossy');
+
+module.exports = function OctetSyslogParser(onPacket) {
+  var buf = new Buffer(0);
+
+  this.feed = function(data) {
+    var newbuf = new Buffer(buf.length + data.length)
+    buf.copy(newbuf, 0)
+    data.copy(newbuf, buf.length)
+    buf = newbuf
+
+    while((spaceIdx = detectSpace(buf)) > -1) {
+      //console.log("buffer contents")
+      //console.dir(buf.toString())
+      //console.log("detected space at ",spaceIdx)
+      //console.log("lenstring:",buf.toString('utf8', 0, spaceIdx))
+      var frameLen = parseInt(buf.toString('utf8', 0, spaceIdx));
+      var frameEnd = spaceIdx + frameLen + 1;
+
+      if(frameEnd < buf.length + 1) { // we never access the 1 byte that's too much, since...
+        var packet = buf.slice(spaceIdx+1, frameEnd) // .. the byte count is exclusive
+        //console.log("get",packet)
+        //console.dir(packet.toString())
+        onPacket(parseSyslog(packet));
+
+        buf = buf.slice(frameEnd, buf.length)
+      } else {
+        //console.log("incomplete packet")
+        break;
+      }
+    }
+    //console.log("no more packets, remaining buffer:")
+    //console.log("buffer contents")
+    //console.dir(buf.toString())
+  }
+
+  function detectSpace(buf) {
+    var spaceIdx = -1;
+    for(var i = 0; i < buf.length; i++) {
+      if(buf[i] == 0x20) {
+        spaceIdx = i;
+        break;
+      }
+    }
+    return spaceIdx;
+  }
+
+  function parseSyslog(buf) {
+    return glossy.Parse.parse(buf.toString('utf8', 0));
+  }
+}
---
+++ send.js
@@ -0,0 +1,100 @@
+var syslogProducer = require('glossy').Produce; // or wherever glossy lives
+var glossy = new syslogProducer();
+
+var OctetSyslogParser = require('./octet_syslog_parser')
+
+var queued = [];
+
+function send(msg) {
+  console.log(glossy.produce(msg));
+}
+
+const bufferFor = 1000;
+
+function flush() {
+  var next;
+
+  var now = new Date().getTime();
+  var sendUpto = -1;
+  var timeOrderedQueue = queued.reverse();
+  for(var i=0; i < timeOrderedQueue.length; i++) {
+    if(timeOrderedQueue[i].ingestTs+bufferFor < now) {
+      sendUpto = i;
+    }
+  }
+
+  for(var i=0; i <= sendUpto; i++) {
+    send(timeOrderedQueue.shift())
+  }
+
+  queued = timeOrderedQueue.reverse()
+
+  clearTimer();
+  setTimer();
+}
+
+
+var flushTimeout;
+
+function clearTimer() {
+  if(flushTimeout) {
+    clearTimeout(flushTimeout);
+    flushTimeout = null;
+  }
+}
+
+function setTimer() {
+  if(!flushTimeout) flushTimeout = setTimeout(flush, bufferFor);
+}
+
+function queueMessage(parsedMessage) {
+  parsedMessage.date = parsedMessage.time; // because lol glossy
+
+  parsedMessage.ts = parsedMessage.date.getTime()
+  parsedMessage.ingestTs = new Date().getTime()
+
+  var insertedAt = -1;
+  for(var i = 0; i < queued.length; i++) {
+    if(queued[i].ts < parsedMessage.ts) {
+      queued.splice(i, 0, parsedMessage);
+      insertedAt = i;
+      break;
+    }
+  }
+
+  if(insertedAt == -1) {
+    insertedAt = queued.length;
+    queued.push(parsedMessage);
+  }
+
+  if(insertedAt > 0) {
+    console.log("reordered", parsedMessage.message)
+  }
+
+  setTimer();
+}
+
+var p2 = new OctetSyslogParser(queueMessage);
+p2.feed(new Buffer(require('fs').readFileSync('./dump', "utf8")));
+
+var net = require('net');
+var server = net.createServer(function (socket) {
+  var p = new OctetSyslogParser(queueMessage);
+
+  socket.on('data', function(data) {
+    p.feed(data);
+  })
+
+  socket.on('end', function(){
+    console.log('client disconnected');
+  });
+})
+
+if(process.argv.length != 3) {
+  console.log("usage: node bla.js <port>")
+  process.exit(1);
+}
+
+var port = parseInt(process.argv[2]);
+server.listen(port, "0.0.0.0");
+console.log("listening on",port)
